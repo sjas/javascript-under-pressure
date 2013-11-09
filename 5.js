4ac36459f1b992376b37a4ed2a86ad225a357609
sjas <teilzeittresor@web.de>
2013-11-09 22:04:38 +0100

    holymotherofgod

--- 5.js
+++ 5.js
@@ -2,7 +2,19 @@ function arraySum(i) {
     
     // i will be an array, containing integers, strings and/or arrays like itself.
     // Sum all the integers you find, anywhere in the nest of arrays.
-    
-    
+
+    return i.reduce(
+        function(total, i)
+        {
+            if (typeof i == 'number') 
+            {
+                return total + i;
+            } 
+            else if (i.reduce) 
+            {
+                return total + arraySum(i);
+            }
+            return total;
+        }, 0);
     
 }
