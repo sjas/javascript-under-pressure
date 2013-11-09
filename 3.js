59cbd9974e823fe04e207fb1d9bf6600cb3cc6d4
sjas <teilzeittresor@web.de>
2013-11-09 21:32:20 +0100

    cleaner solution here, too

--- 3.js
+++ 3.js
@@ -3,9 +3,6 @@ function getFileExtension(i) {
     // i will be a string, but it may not have a file extension.
     // return the file extension (with no period) if it has one, otherwise false
 
-    if ( new RegExp("(\\w*\\.)*\\w*\\.\\w\\w\\w").test(i) ) { 
-        return i.match("\\w+$")
-    }
-    else return false
+     return (new RegExp("(\\w*\\.)*\\w*\\.\\w\\w\\w").test(i)) ? i.match("\\w+$") : false;
     
 }
